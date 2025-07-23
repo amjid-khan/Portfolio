8880994d3c9b230e249a3190c8c777e5bf89da70
Amjid khan <amjidkurrmywal@gmail.com>
2025-07-23 16:29:41 +0500

    more changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'https:http://silly-selkie-ac96cf.netlify.app',
+    origin: 'http://silly-selkie-ac96cf.netlify.app',
     credentials: true,
 }));
 
