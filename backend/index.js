bc92f2c2e861ca43c9664494c6652e3c7402d050
Amjid khan <amjidkurrmywal@gmail.com>
2025-07-28 12:58:31 +0500

    some changes for deployemnt

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// dev.js (for testing locally)
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,13 +10,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/api/portfolio", messageRouter);
 
-// Connect DB & Start Server
 connectDb();
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is running on port:", process.env.PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running locally on port", PORT);
 });
