be2516f412019dd761d372f3f4f9d22630d757e8
Amjid khan <amjidkurrmywal@gmail.com>
2025-07-28 23:06:23 +0500

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,20 @@
+// backend/index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import messageRouter from "./routes/messageRouter.js";
+import serverless from "serverless-http";
 
 dotenv.config();
 
 const app = express();
 
-
-
 app.use(cors());
-
 app.use(express.json());
 
-
+connectDb();
 app.use("/api/portfolio", messageRouter);
 
-connectDb();
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log("Server running on port", PORT);
-});
+//For Vercel (ESM format)
+export const handler = serverless(app);
