2a6aa321fa06aba89a5ff9b98b0f3c7169cf10f8
Amjid khan <amjidkurrmywal@gmail.com>
2025-07-28 13:06:57 +0500

    fix some bugs

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-// dev.js (for testing locally)
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -10,17 +10,16 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-<<<<<<< HEAD
+
 app.use(cors());
-=======
+
 app.use(cors(
     {
-        orgin : "https://deploye-mern-stack-app",
-        method : ["POST" , "GET"],
-        credentials : true
+        orgin: "https://deploye-mern-stack-app",
+        method: ["POST", "GET"],
+        credentials: true
     }
 ))
->>>>>>> d3f25ae9d4e6839dac07c099f6df19c738649d0a
 
 app.use("/api/portfolio", messageRouter);
 
