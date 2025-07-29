7525b03e91d99b361969dc4aeb25aad4e6ee75f0
Amjid khan <amjidkurrmywal@gmail.com>
2025-07-29 11:34:03 +0500

    change apis with domains

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:3000, https://your-frontend.vercel.app",
+        origin: "http://localhost:3000, https:portfolio-frontend-jet-tau.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
